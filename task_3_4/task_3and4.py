d2078ee3b49bd63f266c7c9e1d1688b2e1773438
Tutifruit19 <aurelien.henon@orange.fr>
2022-07-20 15:08:31 +0200

    Normalisation de1995 a 2020

--- task_3_4/task_3and4.py
+++ task_3_4/task_3and4.py
@@ -70,14 +70,16 @@ def fusion_open_SR_table(filename_1,filename_2):
         while result[i][0][l] !=" ":
             l = l+1
         SR_name_receptors.append(result[i][0][:l])
-    print(SR_name_receptors)
     return [SR_name_emitters,SR_name_receptors,result,filename_1,filename_2]
 
 
 def making_output_3(tab,choice_3bis,choice_4,type_pol,method,first_year,last_year):
     name_list_emitters = tab[0]
     name_list_receptors = tab[1]
-    result = normalization(open_SR_tab("data/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
+    if choice_0 == "yes":
+        result = normalization(open_SR_tab("data/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
+    elif choice_0 == "no":
+        result = normalization(fusion_open_SR_table("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
     file = open("output_SR.txt","w")
     file.write(choice_3bis+" normalized with the "+choice_1bis+" of the 2016-2020 meteorology.")
     file = open("output_SR.txt","a")
@@ -102,7 +104,10 @@ def making_output_3(tab,choice_3bis,choice_4,type_pol,method,first_year,last_yea
 def making_output_4(tab,receptors_name,choice_3bis,choice_4,type_pol,method,first_year,last_year):
     name_list_emitters = tab[0]
     name_list_receptors_all = tab[1]
-    result = normalization(open_SR_tab("data/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
+    if choice_0 == "yes":
+        result = normalization(open_SR_tab("data/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
+    elif choice_0 == "no":
+        result = normalization(fusion_open_SR_table("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,method,first_year,last_year)
     index_receptors = []
     receptors_name_list = receptors_name.split(",")
     for i in range(len(receptors_name_list)):
@@ -163,8 +168,8 @@ def making_output_2(tab,receptors_name):
         else:
             pass
 
-def normalization(tab,type_pol,method,first_year,last_year):
-    emi_reduced_nitrogen_2016 = []
+def normalization(tab,type_pol,method,first_year_str,last_year_str):
+    """emi_reduced_nitrogen_2016 = []
     emi_reduced_nitrogen_2017 = []
     emi_reduced_nitrogen_2018 = []
     emi_reduced_nitrogen_2019 = []
@@ -173,18 +178,38 @@ def normalization(tab,type_pol,method,first_year,last_year):
     emi_oxidised_nitrogen_2017 = []
     emi_oxidised_nitrogen_2018 = []
     emi_oxidised_nitrogen_2019 = []
-    emi_oxidised_nitrogen_2020 = []
+    emi_oxidised_nitrogen_2020 = []"""
+    first_year = int(first_year_str)
+    last_year = int(last_year_str)
 
+    emi_reduced_nitrogen = []
+    emi_oxidised_nitrogen = []
     number_year = (last_year - first_year) + 1
     for p in range(number_year):
+        emi_reduced_nitrogen.append([])
+        emi_oxidised_nitrogen.append([])
 
-    tab_reduced_nitrogen_2016 = open_SR_tab("data/reduced_nitrogen_2016.csv")
-    for i in range(1,np.shape(tab_reduced_nitrogen_2016[2])[1]):
-        if tab_reduced_nitrogen_2016[2][-1][i] == "":
-            emi_reduced_nitrogen_2016.append(0.0)
-        else:
-            emi_reduced_nitrogen_2016.append(float(tab_reduced_nitrogen_2016[2][-1][i]))
-    tab_reduced_nitrogen_2017 = open_SR_tab("data/reduced_nitrogen_2017.csv")
+    compteur = 0
+    for p in range(first_year,last_year+1):
+        tab_reduced_nitrogen = open_SR_tab("data/data_emi_normalization/"+str(p)+"_reduced_nitrogen.csv")
+        for i in range(1,np.shape(tab_reduced_nitrogen[2])[1]):
+            if tab_reduced_nitrogen[2][-1][i] == "":
+                emi_reduced_nitrogen[compteur].append(0.0)
+            else:
+                emi_reduced_nitrogen[compteur].append(float(tab_reduced_nitrogen[2][-1][i]))
+        compteur = compteur + 1
+    compteur = 0
+    for p in range(first_year,last_year+1):
+        tab_oxidised_nitrogen = open_SR_tab("data/data_emi_normalization/"+str(p)+"_oxidised_nitrogen.csv")
+        for i in range(1,np.shape(tab_oxidised_nitrogen[2])[1]):
+            if tab_oxidised_nitrogen[2][-1][i] == "":
+                emi_oxidised_nitrogen[compteur].append(0.0)
+            else:
+                emi_oxidised_nitrogen[compteur].append(float(tab_oxidised_nitrogen[2][-1][i]))
+        compteur = compteur + 1
+
+
+    """tab_reduced_nitrogen_2017 = open_SR_tab("data/reduced_nitrogen_2017.csv")
     for i in range(1,np.shape(tab_reduced_nitrogen_2017[2])[1]):
         if tab_reduced_nitrogen_2017[2][-1][i] == "":
             emi_reduced_nitrogen_2017.append(0.0)
@@ -237,9 +262,23 @@ def normalization(tab,type_pol,method,first_year,last_year):
         if tab_oxidised_nitrogen_2020[2][-1][i] == "":
             emi_oxidised_nitrogen_2020.append(0.0)
         else:
-            emi_oxidised_nitrogen_2020.append(float(tab_oxidised_nitrogen_2020[2][-1][i]))
+            emi_oxidised_nitrogen_2020.append(float(tab_oxidised_nitrogen_2020[2][-1][i]))"""
+    SR_tab = []
+    result_jurek = []
+    if choice_0 == "no":
+        for p in range(first_year,last_year+1):
+            SR_tab_tempo = fusion_open_SR_table("data/data_jurek_helcom/"+type_pol+"_"+str(p)+".csv","data/data_jurek_ospar/"+type_pol+"_"+str(p)+".csv")
+            result_jurek_tempo = convert(SR_tab_tempo[2])
+            SR_tab.append(SR_tab_tempo)
+            result_jurek.append(result_jurek_tempo)
+    elif choice_0 == "yes":
+        for p in range(first_year,last_year+1):
+            SR_tab_tempo = open_SR_tab("data/"+type_pol+"_"+str(p)+".csv")
+            result_jurek_tempo = convert(SR_tab_tempo[2])
+            SR_tab.append(SR_tab_tempo)
+            result_jurek.append(result_jurek_tempo)
 
-    SR_tab_2016 = open_SR_tab("data/"+type_pol+"_2016.csv")
+    """SR_tab_2016 = fusion_open_SR_tab("data/data_jurek_helcom/"+type_pol+"_2016.csv","data/data_jurek_ospar/"+type_pol+"_2016.csv")
     result_2016 = convert(SR_tab_2016[2])
     SR_tab_2017 = open_SR_tab("data/"+type_pol+"_2017.csv")
     result_2017 = convert(SR_tab_2017[2])
@@ -248,14 +287,38 @@ def normalization(tab,type_pol,method,first_year,last_year):
     SR_tab_2019 = open_SR_tab("data/"+type_pol+"_2019.csv")
     result_2019 = convert(SR_tab_2019[2])
     SR_tab_2020 = open_SR_tab("data/"+type_pol+"_2020.csv")
-    result_2020 = convert(SR_tab_2020[2])
+    result_2020 = convert(SR_tab_2020[2])"""
 
-    tc_2016 = np.zeros((np.shape(result_2016)))
+    tc = []
+    compteur = 0
+    for p in range(first_year,last_year+1):
+        tc_tempo = np.zeros((np.shape(result_jurek[compteur])))
+        tc.append(tc_tempo)
+        compteur = compteur + 1
+
+    """tc_2016 = np.zeros((np.shape(result_2016)))
     tc_2017 = np.zeros((np.shape(result_2017)))
     tc_2018 = np.zeros((np.shape(result_2018)))
     tc_2019 = np.zeros((np.shape(result_2019)))
-    tc_2020 = np.zeros((np.shape(result_2020)))
+    tc_2020 = np.zeros((np.shape(result_2020)))"""
     if type_pol == "oxidised_nitrogen" or type_pol == "dry_oxidised_nitrogen" or type_pol == "wet_oxidised_nitrogen":
+        for p in range(np.shape(tc)[0]):
+            for i in range(np.shape(tc)[1]):
+                for j in range(np.shape(tc)[2]):
+                    if emi_oxidised_nitrogen[p][j] != 0:
+                        tc[p][i][j] = result_jurek[p][i][j]/emi_oxidised_nitrogen[p][j]
+                    else:
+                        tc[p][i][j] = float("nan")
+    elif type_pol == "reduced_nitrogen" or type_pol == "dry_reduced_nitrogen" or type_pol == "wet_reduced_nitrogen":
+        for p in range(np.shape(tc)[0]):
+            for i in range(np.shape(tc)[1]):
+                for j in range(np.shape(tc)[2]):
+                    if emi_reduced_nitrogen[p][j] != 0:
+                        tc[p][i][j] = result_jurek[p][i][j]/emi_reduced_nitrogen[p][j]
+                    else:
+                        tc[p][i][j] = float("nan")
+
+    """if type_pol == "oxidised_nitrogen" or type_pol == "dry_oxidised_nitrogen" or type_pol == "wet_oxidised_nitrogen":
         for i in range(np.shape(tc_2016)[0]):
             for j in range(np.shape(tc_2016)[1]):
                 if emi_oxidised_nitrogen_2016[j] !=0:
@@ -316,17 +379,28 @@ def normalization(tab,type_pol,method,first_year,last_year):
                 if emi_reduced_nitrogen_2020[j] != 0:
                     tc_2020[i][j] = result_2020[i][j]/emi_reduced_nitrogen_2020[j]
                 else:
-                    tc_2020[i][j] = float("nan")
+                    tc_2020[i][j] = float("nan")"""
 
     name_list_emitters = tab[0]
     name_list_receptors_all = tab[1]
     result = convert(tab[2])
-    normalized_2016 = np.zeros((np.shape(result)))
+
+    normalized = []
+    for p in range(first_year,last_year+1):
+        normalized.append(np.zeros((np.shape(result))))
+
+    """normalized_2016 = np.zeros((np.shape(result)))
     normalized_2017 = np.zeros((np.shape(result)))
     normalized_2018 = np.zeros((np.shape(result)))
     normalized_2019 = np.zeros((np.shape(result)))
-    normalized_2020 = np.zeros((np.shape(result)))
-    for i in range(np.shape(normalized_2016)[0]):
+    normalized_2020 = np.zeros((np.shape(result)))"""
+
+    for p in range(np.shape(normalized)[0]):
+        for i in range(np.shape(normalized)[1]):
+            for j in range(np.shape(normalized)[2]):
+                normalized[p][i][j] = tc[p][i][j]*result[i][j]
+
+    """for i in range(np.shape(normalized_2016)[0]):
         for j in range(np.shape(normalized_2016)[1]):
             normalized_2016[i][j] = tc_2016[i][j]*result[i][j]
     for i in range(np.shape(normalized_2017)[0]):
@@ -340,8 +414,10 @@ def normalization(tab,type_pol,method,first_year,last_year):
             normalized_2019[i][j] = tc_2019[i][j]*result[i][j]
     for i in range(np.shape(normalized_2020)[0]):
         for j in range(np.shape(normalized_2020)[1]):
-            normalized_2020[i][j] = tc_2020[i][j]*result[i][j]
-    substract = [normalized_2016,normalized_2017,normalized_2018,normalized_2019,normalized_2020]
+            normalized_2020[i][j] = tc_2020[i][j]*result[i][j]"""
+
+
+    """substract = [normalized_2016,normalized_2017,normalized_2018,normalized_2019,normalized_2020]
     somme = np.zeros((np.shape(normalized_2020)))
     median_tab = np.empty(np.shape(somme),dtype=list)#start the stuff
     for i in range(np.shape(median_tab)[0]):
@@ -383,8 +459,28 @@ def normalization(tab,type_pol,method,first_year,last_year):
     elif method =="median":
         for i in range(np.shape(normalized_table)[0]):
             for j in range(np.shape(normalized_table)[1]):
-                normalized_table[i][j] = sc.median(median_tab[i][j])
+                normalized_table[i][j] = sc.median(median_tab[i][j])"""
 
+    if method == "average":
+        normalized_table = np.zeros((np.shape(normalized)[1:]))
+        for i in range(np.shape(normalized)[1]):
+            for j in range(np.shape(normalized)[2]):
+                for p in range(np.shape(normalized)[0]):
+                    normalized_table[i][j]=normalized_table[i][j]+normalized[p][i][j]
+                normalized_table[i][j] = normalized_table[i][j]/(np.shape(normalized)[0])
+    elif method == "median":
+        median_tab = np.empty(np.shape(normalized)[1:],dtype=list)
+        for i in range(np.shape(median_tab)[0]):
+            for j in range(np.shape(median_tab)[1]):
+                median_tab[i][j] = []
+        for i in range(np.shape(median_tab)[0]):
+            for j in range(np.shape(median_tab)[1]):
+                for p in range(np.shape(normalized)[0]):
+                    median_tab[i][j].append(normalized[p][i][j])
+        normalized_table = np.zeros((np.shape(normalized)[1:]))
+        for i in range(np.shape(normalized)[1]):
+            for j in range(np.shape(normalized)[2]):
+                normalized_table[i][j] = sc.median(median_tab[i][j])
     return normalized_table
 
 #print(normalization(open_SR_tab("/home/aurelienh/task_3and4/data/dry_oxidised_nitrogen_2018.csv"),"oxidised_nitrogen","average"))
@@ -584,7 +680,7 @@ elif choice_0 == "no":
                 print("Incorrect input, please try again")
             else:
                 token_4a = 1
-        print("Select the last year between 2016 and 2020: (> first year)")
+        print("Select the last year between 1995 and 2020: (> first year)")
         token_4a = 0
         while token_4a == 0:
             last_year = input()
@@ -624,14 +720,14 @@ elif choice_0 == "no":
             token_3 = 0
             print("Incorrect input, please try again:")
         else:
-            if choice_3 == "oxidised nitrogen":
-                choice_3bis = "oxidised_nitrogen"
-            elif choice_3 == "dry oxidised nitrogen":
+            #if choice_3 == "oxidised nitrogen":
+               # choice_3bis = "oxidised_nitrogen"
+            if choice_3 == "dry oxidised nitrogen":
                 choice_3bis = "dry_oxidised_nitrogen"
             elif choice_3 == "wet oxidised nitrogen":
                 choice_3bis = "wet_oxidised_nitrogen"
-            elif choice_3 == "reduced nitrogen":
-                choice_3bis = "reduced_nitrogen"
+            #elif choice_3 == "reduced nitrogen":
+                #choice_3bis = "reduced_nitrogen"
             elif choice_3 == "dry reduced nitrogen":
                 choice_3bis = "dry_reduced_nitrogen"
             elif choice_3 == "wet reduced nitrogen":
@@ -658,6 +754,8 @@ elif choice_0 == "no":
             making_output_2(fusion_open_SR_table("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),str_receptors)
     elif choice_1 == "normalized":
         if choice_2 == "yes":
-            making_output_3(fusion_open_SR_tab("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,choice_4,choice_3bis,choice_1bis,first_year,last_year)
+            making_output_3(fusion_open_SR_table("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),choice_3bis,choice_4,choice_3bis,choice_1bis,first_year,last_year)
+        elif choice_2 == "no":
+            making_output_4(fusion_open_SR_table("data/data_jurek_helcom/"+choice_3bis+"_"+choice_4+".csv","data/data_jurek_ospar/"+choice_3bis+"_"+choice_4+".csv"),str_receptors,choice_3bis,choice_4,choice_3bis,choice_1bis,first_year,last_year)
 
 #for annee choisie et with les meteorologies choisies
